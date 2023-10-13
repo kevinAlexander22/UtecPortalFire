6880b1eed686a7031f09856635046055358c722f
kevinAlexander22 <70768955+kevinAlexander22@users.noreply.github.com>
2023-10-12 22:24:54 -0600

    commit_4
    actualizando commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-/*  cambios en main con acceso a firebase */ //actualizado
+/*  cambios en main con acceso a firebase */ //actualizando firebase
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
