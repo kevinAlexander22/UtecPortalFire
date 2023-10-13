6e54ad580211139f4d034302f5489359d73bef14
Keviin25 <2547822019@mail.utec.edu.sv>
2023-10-12 22:15:51 -0600

    prueba3
    prueba3 3

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,23 @@ import 'package:portalutec/pages/HomePage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-/*  cambios en main con acceso a firebase */
+
+/*  cambios en main con acceso a firebase */ //actualizado
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp( MyApp());
+  runApp(MyApp());
 }
-  
-      //declarando los controles 
-  //TextEditingController userController = TextEditingController();
- // TextEditingController passwordController = TextEditingController();
+
+//declarando los controles
+//TextEditingController userController = TextEditingController();
+// TextEditingController passwordController = TextEditingController();
 class MyApp extends StatelessWidget {
   final TextEditingController userController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-    String getUserEntered() {
+  String getUserEntered() {
     return userController.text;
   }
 
@@ -47,150 +48,151 @@ class MyApp extends StatelessWidget {
       }
     }
   }*/
-  
-  
+
   Widget build(BuildContext context) {
-    
     return MaterialApp(
-      home: Scaffold(      
-        body: Column(
-              children:[
-                cuerpo(context),//se llama la funcion del cuerpo           
-                 Screen1(
-                  userController: userController,
-                  passwordController: passwordController,
-                  ),  
-              ] 
+      home: Scaffold(
+        body: Column(children: [
+          cuerpo(context), //se llama la funcion del cuerpo
+          Screen1(
+            userController: userController,
+            passwordController: passwordController,
+          ),
+        ]),
+      ),
+    );
+  }
+
+  //en esta funcion se va devolver un widget
+  Widget cuerpo(BuildContext context) {
+    return Container(
+      color: Color.fromARGB(255, 255, 255, 255), // Color de fondo
+      child: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: double
+                      .infinity, // Ajusta el ancho de la imagen al ancho de la pantalla
+                  height: MediaQuery.of(context).size.height *
+                      0.5, // Altura de la imagen
+                  child: Image.asset(
+                    "assets/logo_utec.png",
+                    fit: BoxFit.contain, // Ajusta la imagen sin distorsionarla
+                  ),
+                ),
+                SizedBox(
+                    height: 5), // Espacio entre la imagen y otros elementos
+                Align(
+                  alignment: Alignment.center,
+                  child: name(),
+                ),
+                SizedBox(height: 15),
+                Align(
+                  alignment: Alignment.center,
+                  child: user(userController),
+                ),
+                SizedBox(height: 15),
+                Align(
+                  alignment: Alignment.center,
+                  child: password(passwordController),
+                ),
+                SizedBox(height: 20),
+                Align(
+                  alignment: Alignment.center,
+                  child: Screen1(
+                    userController: userController,
+                    passwordController: passwordController,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Align(
+                  alignment: Alignment.center,
+                  child: savePin(),
+                ),
+                SizedBox(height: 20),
+                Align(
+                  alignment: Alignment.center,
+                  child: invitado(),
+                ),
+                SizedBox(height: 22),
+                // Espacio adicional al final
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 
-
-  //en esta funcion se va devolver un widget
-Widget cuerpo(BuildContext context) {
-  
-  return Container(
-    color: Color.fromARGB(255, 255, 255, 255), // Color de fondo
-    child: SingleChildScrollView(
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: double.infinity, // Ajusta el ancho de la imagen al ancho de la pantalla
-                height: MediaQuery.of(context).size.height * 0.5, // Altura de la imagen
-                child: Image.asset(
-                  "assets/logo_utec.png",
-                  fit: BoxFit.contain, // Ajusta la imagen sin distorsionarla
-                ),
-              ),
-              SizedBox(height: 5), // Espacio entre la imagen y otros elementos
-              Align(
-                alignment: Alignment.center,
-                child: name(),
-              ),
-              SizedBox(height: 15),
-              Align(
-                alignment: Alignment.center,
-                child: user(userController),
-              ),
-              SizedBox(height: 15),
-              Align(
-                alignment: Alignment.center,
-                child: password(passwordController),
-              ),
-              SizedBox(height: 20),
-              Align(
-                alignment: Alignment.center,
-                child: Screen1(userController: userController,
-                  passwordController: passwordController,),
-              ),
-              SizedBox(height: 20),
-              Align(
-                alignment: Alignment.center,
-                child: savePin(),
-              ),
-               SizedBox(height: 20),
-              Align(
-                alignment: Alignment.center,
-                child: invitado(),
-              ),
-              SizedBox(height: 22),
-              // Espacio adicional al final
-            ],
-          ),
-        ),
-      ),
-    ),
-  );
-}
-
-
-   //se declara widget nombre box
-   Widget name(){
-      return Text("Sing in",style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0),fontSize: 19,fontWeight: FontWeight.bold),);
-   }
-      //widget para user box con un TextField
-
-
+  //se declara widget nombre box
+  Widget name() {
+    return Text(
+      "Sing in",
+      style: TextStyle(
+          color: const Color.fromARGB(255, 0, 0, 0),
+          fontSize: 19,
+          fontWeight: FontWeight.bold),
+    );
+  }
+  //widget para user box con un TextField
 
 //para link de recuperacion contraseña
-Widget savePin(){
- return TextButton(
- onPressed: (){},
- child: Text("Recuperar Clave"));
-}
-Widget user(TextEditingController controller){
-  return Container(
-    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-    child: TextField(
-      controller: userController, // Asigna el controlador
-      decoration: InputDecoration(
-        border: UnderlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0),
+  Widget savePin() {
+    return TextButton(onPressed: () {}, child: Text("Recuperar Clave"));
+  }
+
+  Widget user(TextEditingController controller) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+      child: TextField(
+        controller: userController, // Asigna el controlador
+        decoration: InputDecoration(
+          border: UnderlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          hintText: "User",
+          fillColor: Color.fromARGB(255, 240, 233, 233),
+          filled: true,
         ),
-        hintText: "User",
-        fillColor: Color.fromARGB(255, 240, 233, 233),
-        filled: true,
       ),
-    ),
-  );
-}
-Widget password(TextEditingController controller) {
-  return Container(
-    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-    child: TextField(
-      controller: passwordController, // Asigna el controlador
-      obscureText: true,
-      decoration: InputDecoration(
-        border: UnderlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0),
+    );
+  }
+
+  Widget password(TextEditingController controller) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+      child: TextField(
+        controller: passwordController, // Asigna el controlador
+        obscureText: true,
+        decoration: InputDecoration(
+          border: UnderlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          hintText: "PassWord",
+          fillColor: Color.fromARGB(255, 240, 233, 233),
+          filled: true,
         ),
-        hintText: "PassWord",
-        fillColor: Color.fromARGB(255, 240, 233, 233),
-        filled: true,
       ),
-    ),
-  );
+    );
+  }
 }
 
-}
 class Screen1 extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final TextEditingController userController;
   final TextEditingController passwordController;
-   Screen1({
+  Screen1({
     required this.userController,
     required this.passwordController,
   });
 
- // TextEditingController userController = TextEditingController();
+  // TextEditingController userController = TextEditingController();
 //  TextEditingController passwordController = TextEditingController();
-
 
   @override
   Widget build(BuildContext context) {
@@ -205,15 +207,18 @@ class Screen1 extends StatelessWidget {
       onPressed: () async {
         try {
           // Obtener los valores ingresados por el usuario
-         // String userEntered = userController.text;//"admin@gmail.com";
+          // String userEntered = userController.text;//"admin@gmail.com";
           //String passwordEntered = passwordController.text;//"123456";
 
- // Obtener los valores ingresados por el usuario
-       // Obtener los valores ingresados por el usuario
+          // Obtener los valores ingresados por el usuario
+          // Obtener los valores ingresados por el usuario
           String userEntered = userController.text;
           String passwordEntered = passwordController.text;
           // Obtener los datos del documento en Firestore
-          DocumentSnapshot userSnapshot = await _firestore.collection('usuarios').doc('idUsuario').get(); //userEntered
+          DocumentSnapshot userSnapshot = await _firestore
+              .collection('usuarios')
+              .doc('idUsuario')
+              .get(); //userEntered
 
           if (userSnapshot.exists) {
             // Obtener los datos del documento
@@ -221,7 +226,8 @@ class Screen1 extends StatelessWidget {
             String storedPassword = userSnapshot.get('contraseña');
 
             // Realizar la comparación con los valores ingresados por el usuario
-            if (_validateUser(userEntered, storedUser) && _validatePassword(passwordEntered, storedPassword)) {
+            if (_validateUser(userEntered, storedUser) &&
+                _validatePassword(passwordEntered, storedPassword)) {
               // Si las credenciales son correctas, realizar la autenticación con Firebase
               await _auth.signInWithEmailAndPassword(
                 email: userEntered,
@@ -261,39 +267,27 @@ class Screen1 extends StatelessWidget {
   }
 }
 
-
-
-   //campo para Password
-
+//campo para Password
 
 //agreagnod imagen de inicio login
 
- //creando la clase para el evento boton
+//creando la clase para el evento boton
 
- class invitado extends StatelessWidget {
+class invitado extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-     return TextButton(
-  style: ElevatedButton.styleFrom( 
-     shape: RoundedRectangleBorder(
-    ),
-         // primary: const Color.fromARGB(255, 24, 25, 25),
+    return TextButton(
+      style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(),
+          // primary: const Color.fromARGB(255, 24, 25, 25),
           onPrimary: Color.fromARGB(255, 0, 0, 0),
-         
-      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10)),//modificar tamaño del boton 
+          padding: EdgeInsets.symmetric(
+              horizontal: 30, vertical: 10)), //modificar tamaño del boton
       child: Text("Invitado"),
-      onPressed: ()=> {
-         Navigator.push(
-          context, 
-          MaterialPageRoute(builder: (context)=> HomePage())
-         )
+      onPressed: () => {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomePage()))
       },
-  );
+    );
   }
 }
-
-
-
-
-
-
